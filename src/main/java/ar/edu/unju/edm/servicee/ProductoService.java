1ffd5f9d363afaadfefe491b0e357913e7a2753d
Santi <Santi@DESKTOP-2IHDDJI>
2021-05-04 17:25:29 -0300

    Inicializacion del proyecto

---
+++ src/main/java/ar/edu/unju/edm/servicee/ProductoService.java
@@ -0,0 +1,18 @@
+package ar.edu.unju.edm.servicee;
+
+import java.util.List;
+
+import ar.edu.unju.edm.model.Producto;
+
+//@Service
+public interface ProductoService {
+	
+	public void guardarProducto(Producto unProducto);
+	public Producto crearProducto();
+	public void modificarProducto(Producto productoAModificar);
+	public void eliminarProducto(Producto productoAEliminar);
+	public Producto obtenerUnProducto(String nombreProducto);
+	public List<Producto> obtenerTodosProductos();
+	public Producto obtenerProductoNuevo();
+	public Producto obtenerUltimoProducto();
+}
